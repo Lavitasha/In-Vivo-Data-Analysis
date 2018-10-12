adbe94cd116e50577aa91546485ceba5fdd5d42e
Emmett Thompson <Hester@eduroam-int-dhcp-97-99-207.ucl.ac.uk>
2018-10-12 15:16:38 +0100

    signal data

--- Signaldata.py
+++ Signaldata.py
@@ -6,22 +6,67 @@ import matplotlib.pyplot as plt
 sampling_interval = 0.0001
 file_path = '/Users/Hester/Desktop/In-Vivo-Data-Analysis/data/studentCourse_command.csv'
 
+
 def main():
     # IMPORT and read data
     data_file_path = file_path
     data = pd.read_csv(data_file_path)
     data = data['command'].values
 
-    movement_first_pnt_idx, movement_last_pnt_idx = get_movmenent_range(data)
-
-    plot_raw_data(data, movement_first_pnt_idx, movement_last_pnt_idx, sampling_interval=sampling_interval)
-
+    movement_first_pnt_idx, movement_last_pnt_idx = get_movement_range(data)
     differential, direction = get_direction(data)
 
+    plot_raw_data(data, movement_first_pnt_idx, movement_last_pnt_idx, sampling_interval=sampling_interval)
     plot_differential_plot_direction(differential, direction, sampling_interval)
 
 
+def get_direction(data):
+    """
+    :param data: Movement command waveform
+    :return: Gives
+     direction: an array of 0 and 1's defining turing direction at a certain time point
+     differential: first derivative of the movement command waveform
+    """
+    differential = np.diff(data)
+    direction = np.zeros(differential.shape, dtype=np.int64)
+    direction[differential > 0] = 1
+    direction[differential < 0] = -1
+    return differential, direction
+
+
+def get_movement_range(data):
+    """
+    :param np.array data: Data is the movement command waveform
+    :return:
+    """
+    is_moving = np.where(data != 0)[0]
+    movement_first_pnt_idx = (is_moving[0])
+    movement_last_pnt_idx = (is_moving[-1])
+    return movement_first_pnt_idx, movement_last_pnt_idx
+
+
+def plot_raw_data(data, movement_first_pnt_idx, movement_last_pnt_idx, sampling_interval=0.0001):
+    """
+    :param data: Movement command waveform
+    :param movement_first_pnt_idx: first non zero movement command waveform index
+    :param movement_last_pnt_idx: last non zero movement command waveform index
+    :param sampling_interval: clock speed of the motor driver
+    :return: Plot showing first and last non zero movement command waveform index
+    """
+    x_axis = np.linspace(0, sampling_interval * len(data), len(data))
+    plt.plot(x_axis, data)
+    plt.axvline(movement_first_pnt_idx * sampling_interval, color='r')
+    plt.axvline(movement_last_pnt_idx * sampling_interval, color='g')
+    plt.show()
+
+
 def plot_differential_plot_direction(differential, direction, sampling_interval):
+    """
+    :param differential: first derivative of movement command waveform
+    :param direction: an array of 0 and 1's defining turing direction at a certain time point
+    :param sampling_interval: clock speed of the motor driver
+    :return: Plots showing first derivative of movement command waveform and direction logic of movement command waveform
+    """
     x_axis = np.linspace(0, sampling_interval * len(differential), len(differential))
     plt.plot(x_axis, differential)
     plt.show()
@@ -29,35 +74,10 @@ def plot_differential_plot_direction(differential, direction, sampling_interval)
     plt.show()
 
 
-def get_direction(data):
-    differential = np.diff(data)
-    direction = np.zeros(differential.shape, dtype=np.int64)
-    direction[differential > 0] = 1
-    direction[differential < 0] = -1
-    return differential, direction
-
-def plot_raw_data(data, movement_first_pnt_idx, movement_last_pnt_idx, sampling_interval=0.0001):
-    x_axis = np.linspace(0, sampling_interval * len(data), len(data))
-    plt.plot(x_axis, data)
-    plt.axvline(movement_first_pnt_idx * sampling_interval, color='r')
-    plt.axvline(movement_last_pnt_idx * sampling_interval, color='g')
-    plt.show()
-
-def get_movmenent_range(data):
-    """
-
-    :param np.array data:
-    :return:
-    """
-    movement_first_pnt_idx = (np.where(data != 0)[0][0])
-    movement_last_pnt_idx = (np.where(data != 0)[0][-1])
-    return movement_first_pnt_idx, movement_last_pnt_idx
-
-
 if __name__ == '__main__':
     main()
 
 
-# direction is an array of 0 and 1's defining turing direction at a certain time point
+# d
 # ti is the index at which the turns start (for the time: ti*0.0001)
 # tf is the index at which the turn ends (for the time: tf*0.0001)
